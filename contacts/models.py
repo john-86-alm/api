3e9837234abf2b47d02ce7fe4d23dd40bb406686
Evg Balkunov <mailjohn@mail.ru>
2026-02-13 16:36:25 +0500

    working API

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    number = models.PositiveIntegerField(unique=True)
+    location = models.CharField(max_length=100, blank=True)
+    user = models.CharField(max_length=150, blank=True)
+    group = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.number} - {self.user} - {self.location} - {self.group}"
